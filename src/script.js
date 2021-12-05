b940b661a7bf5882e27e44e9e4231ab8bff7a2ca
surhonjuraholov <surhon.juraholov@gmail.com>
2021-12-05 18:41:27 -0500

    created html and fetch script

---
+++ src/script.js
@@ -0,0 +1,15 @@
+function fetchMyDegrees(url) {
+    await fetch(url)
+    .then((response) => {
+        let data = response.json()
+        if(response.status === 200){
+            let index = 1;
+            data.degrees.forEach(element => {
+                document.write(
+                    index + '. ${element.type} degree in ${element.major} from ${element.school}, ${element.year_conferred}'
+                )
+                index++;
+            });
+        }
+    })
+}
