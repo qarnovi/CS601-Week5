e34b198f513284251ddfbaaa47f1234ea4567547
surhonjuraholov <surhon.juraholov@gmail.com>
2021-12-05 20:06:19 -0500

    1 more fix

--- src/script.js
+++ src/script.js
@@ -4,7 +4,8 @@ async function fetchMyDegrees() {
         let data = response.json()
         if(response.status === 200){
             let index = 1;
-            data.degrees.forEach(element => {
+            let degrees = data.degrees
+            degrees.forEach(element => {
                 document.write(
                     index + '. ${element.type} degree in ${element.major} from ${element.school}, ${element.year_conferred}'
                 )
